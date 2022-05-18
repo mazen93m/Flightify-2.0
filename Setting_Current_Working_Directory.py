1dfcc6acb7a2eba5ce17002a92c10036d01fdf3c
kcawi <98711993+kcawi@users.noreply.github.com>
2022-05-18 10:35:25 -0400

    Add files via upload

---
+++ Setting_Current_Working_Directory.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May 18 10:29:29 2022
+
+@author: Kim
+"""
+
+#Import module os and Check Working Directory
+import os
+os.getcwd()
+
+#Change working Directory to Code and Data
+os.chdir('C:\\Users\\Kim\\Desktop\\DAEN 690\\Code and Data')
+os.getcwd()
